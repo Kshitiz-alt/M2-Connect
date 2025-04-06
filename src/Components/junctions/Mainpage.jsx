11936e24070abcbb872d2117b9c8011a98364b62
saiki038 <saxenaktz@gmail.com>
2025-04-06 15:28:43 +0530

    modified main page

--- src/Components/junctions/Mainpage.jsx
+++ src/Components/junctions/Mainpage.jsx
@@ -1,31 +1,32 @@
 import { useNavigate } from 'react-router-dom'
 import Content from './Content'
-export default function Mainpage() {
+function Mainpage() {
     const navigate = useNavigate()
 
     return (
         <div>
             <div>
-                <nav className="bg-transparent flex gap-[30rem] items-center">
-                    <img className=" h-[8rem] ml-[15rem] p-2 rounded-full" onClick={() => navigate('/Login')} src="/image.png" alt="" />
-                    <p className="text-5xl m-10 font-bold text-transparent bg-clip-text bg-gradient-to-r from-gray-900 to-white">
+                <nav className="bg-transparent border-2 m-3 rounded-full flex gap-[50rem] items-center max-sm:gap-[1rem] max-sm:m-[1px] ">
+                    <img className=" h-[8rem] p-1 rounded-full" onClick={() => navigate('/Login')} src="/image.png" alt="" />
+                    <p className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-gray-900 to-white max-sm:text-xl ">
                         M2 CONNECT
                     </p>
+                   
                 </nav>
             </div>
-            <div className="m-3 flex">
+            <div className="m-3 flex max-sm:flex-none">
                 <div className="buttons">
-                    <div className='text-black flex flex-col gap-2 w-[18rem]'>
-                        <button onClick={() => navigate('/LatestBollywood')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl" type="submit">
+                    <div className='text-black flex flex-col gap-2 w-[18rem] max-sm:w-[10rem]' >
+                        <button onClick={() => navigate('/LatestBollywood')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl max-sm:text-sm hover:text-3xl hover:w-[18rem] hover:bg-gradient-to-r hover:from-gray-500 hover:to-white" type="submit">
                             Latest Bollywood
-                        </button>
-                        <input onClick={() => navigate('/LatestPunjabi')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl" type="button" value="Latest Punjabi" />
-                        <input onClick={() => navigate('/LatestIndiPOP')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl" type="button" value="Latest IndiPOP" />
-                        <input onClick={() => navigate('/TopSong2024')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl" type="button" value="Tops Songs 2024" />
-                        <input onClick={() => navigate('/Anime')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl" type="submit" value="Anime/Video" />
-                        <input onClick={() => navigate('/HaryanviSongs')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl" type="button" value="Haryanvi Songs" />
-                        <input onClick={() => navigate('/NewRingTones')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl" type="button" value="New Ringtones" />
-                        <input onClick={() => navigate('/NewDJRemix')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl" type="button" value="New DJ Remix" />
+                        </button>   
+                        <input onClick={() => navigate('/LatestPunjabi')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl max-sm:text-sm hover:text-3xl hover:w-[18rem] hover:bg-gradient-to-r hover:from-gray-500 hover:to-white" type="button" value="Latest Punjabi" />
+                        <input onClick={() => navigate('/LatestIndiPOP')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl max-sm:text-sm hover:text-3xl hover:w-[18rem] hover:bg-gradient-to-r hover:from-gray-500 hover:to-white" type="button" value="Latest IndiPOP" />
+                        <input onClick={() => navigate('/TopSong2024')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl max-sm:text-sm hover:text-3xl hover:w-[18rem] hover:bg-gradient-to-r hover:from-gray-500 hover:to-white" type="button" value="Tops Songs 2024" />
+                        <input onClick={() => navigate('/Anime')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl max-sm:text-sm hover:text-3xl hover:w-[18rem] hover:bg-gradient-to-r hover:from-gray-500 hover:to-white" type="submit" value="Anime" />
+                        <input onClick={() => navigate('/HaryanviSongs')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl max-sm:text-sm hover:text-3xl hover:w-[18rem] hover:bg-gradient-to-r hover:from-gray-500 hover:to-white" type="button" value="Haryanvi Songs" />
+                        <input onClick={() => navigate('/NewRingTones')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl max-sm:text-sm hover:text-3xl hover:w-[18rem] hover:bg-gradient-to-r hover:from-gray-500 hover:to-white" type="button" value="New Ringtones" />
+                        <input onClick={() => navigate('/NewDJRemix')} className="border rounded-full p-2 bg-gradient-to-r from-white to-gray-500 font-bold text-2xl max-sm:text-sm hover:text-3xl hover:w-[18rem] hover:bg-gradient-to-r hover:from-gray-500 hover:to-white" type="button" value="New DJ Remix" />
                     </div>
                 </div>
                 <Content />
@@ -33,3 +34,5 @@ export default function Mainpage() {
         </div>
     )
 }
+
+export default Mainpage
